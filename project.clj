11d244ed0bd2b217eefebad8167167e8b7febc6d
Raymond Huang <rymndhng@gmail.com>
2015-07-09 23:11:46 -0700

    bump dependencies

--- project.clj
+++ project.clj
@@ -3,20 +3,23 @@
   :url "github.com/rymndhng/entangle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "0.0-3308"]
+
+                 [org.clojars.rymndhng/clj-diff "1.1.1-SNAPSHOT"]
+
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [clj-diff "1.0.0-SNAPSHOT"]
                  [com.taoensso/timbre "3.4.0"]
+
                  [aleph "0.4.0"]
                  [compojure "1.3.3"]
 
                  ;; frontend
                  [hiccup "1.0.5"]
-                 [org.clojure/clojurescript "0.0-3211"]
                  [figwheel "0.2.7"]]
 
-  :plugins [[lein-cljsbuild "1.0.5"]
-            [lein-figwheel "0.2.7"]]
+  :plugins [[lein-cljsbuild "1.0.6"]
+            [lein-figwheel "0.3.7"]]
 
   :cljsbuild {:builds [{:id "local"
                         :source-paths ["src"]
@@ -24,6 +27,5 @@
                                    :output-dir "resources/public/js/out"
                                    :source-map "resources/publis/js/out.js.map"
                                    :optimizations :none
-                                   :source-maps true}}]
-              }
+                                   :source-maps true}}]}
   )
